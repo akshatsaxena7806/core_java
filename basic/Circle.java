2abc0d70c76f90a055e9ad83440c1e0495e9de98
akshatsaxena7806 <akshatsaxena7806@gmail.com>
2025-06-17 11:16:08 +0530

    done

---
+++ basic/Circle.java
@@ -0,0 +1,15 @@
+package com.rays.oop.abstraction;
+
+public class Circle extends Shape {
+
+	private double radius;
+
+	public double area() {
+		double area =0.00;  
+		return Math.PI*radius*radius;
+		
+	}
+	
+	
+
+}
---
+++ basic/Shape.java
@@ -0,0 +1,13 @@
+package com.rays.oop.abstraction;
+
+public abstract class Shape {
+
+	public abstract double area();
+	
+	private void demo() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
